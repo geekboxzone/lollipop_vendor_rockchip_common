125b74900e1cabe6cc8b44e23992589fd251a5b8
charles.chen <cch@rock-chips.com>
2015-11-24 21:13:11 +0800

    sofia3gr:update bt lib
    Signed-off-by: charles.chen <cch@rock-chips.com>

---
+++ bluetooth/libbtsofia/Android.mk
@@ -0,0 +1,17 @@
+ifeq ($(PRODUCT_BUILD_MODULE), car)
+LOCAL_PATH := $(call my-dir)
+#
+# bluetooth.default.so for target.
+#
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES_32 := x86/bluetooth.default.so
+LOCAL_MODULE:= bluetooth.default
+LOCAL_MODULE_OWNER := rockchip
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
+
+endif
